84ec42c2df30fe6806e00a4e2a8383aa0abd4b9a
Raghunandan Kavi <raghunandankavi2010@gmail.com>
2019-12-24 15:06:09 +0530

    Initial ui and design setup.

---
+++ app/src/main/java/me/raghu/expensetracker/ExpenseInputActivity.java
@@ -0,0 +1,6 @@
+package me.raghu.expensetracker;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ExpenseInputActivity extends AppCompatActivity {
+}
--- app/src/main/java/me/raghu/expensetracker/MainActivity.kt
+++ app/src/main/java/me/raghu/expensetracker/MainActivity.kt
@@ -2,11 +2,15 @@ package me.raghu.expensetracker
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.toolbar.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        setSupportActionBar(toolbar)
+        supportActionBar?.title= "Expense Tracker"
     }
 }

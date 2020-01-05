e2d6ff88a9299ed06a6620395a8f4cf509b75ad8
Raghunandan Kavi <raghunandankavi2010@gmail.com>
2020-01-05 16:50:33 +0530

    Add Date for the user to choose.

---
+++ app/src/main/java/me/raghu/expensetracker/ui/editexpense/DatePicker.kt
@@ -0,0 +1,32 @@
+package me.raghu.expensetracker.ui.editexpense
+
+import android.app.DatePickerDialog
+import android.app.Dialog
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.DialogFragment
+import androidx.lifecycle.ViewModelProviders
+import java.util.*
+
+class DatePicker : DialogFragment(), DatePickerDialog.OnDateSetListener {
+
+    private var model: ShareDateModel? = null
+    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+        model = activity?.let { ViewModelProviders.of(it).get(ShareDateModel::class.java) }
+        val c = Calendar.getInstance()
+        val year = c.get(Calendar.YEAR)
+        val month = c.get(Calendar.MONTH)
+        val day = c.get(Calendar.DAY_OF_MONTH)
+
+        // Create a new instance of DatePickerDialog and return it
+        return DatePickerDialog(activity as AppCompatActivity, this, year, month, day)
+    }
+
+    override fun onDateSet(view: android.widget.DatePicker?, year: Int, month: Int, day: Int) {
+        val c = Calendar.getInstance()
+        c.set(Calendar.YEAR,year)
+        c.set(Calendar.MONTH,month)
+        c.set(Calendar.DAY_OF_MONTH,day)
+        model?.selectedDate?.value = c.time
+    }
+}

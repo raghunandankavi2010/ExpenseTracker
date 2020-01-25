d16f063208f29f64c6af2fa61de721d5f7232182
Raghuandan Kavi <raghuandan2005@gmail.com>
2020-01-25 14:15:54 +0530

    Remove unwanted data read from db.

--- app/src/main/java/me/raghu/expensetracker/db/ExpenseDao.kt
+++ app/src/main/java/me/raghu/expensetracker/db/ExpenseDao.kt
@@ -17,7 +17,7 @@ interface ExpenseDao {
     @Query("DELETE FROM expense WHERE id = :expenseId")
     fun deleteExpense(expenseId:Int)
 
-    @Query("SELECT id,expense_type,remarks,date,SUM(expense_amount) as expense_amount FROM expense WHERE date BETWEEN :from AND :to GROUP BY date ORDER BY date ASC")
+    @Query("SELECT id,date,SUM(expense_amount) as expense_amount FROM expense WHERE date BETWEEN :from AND :to GROUP BY date ORDER BY date ASC")
     fun expenseInRange(from: Date, to: Date): LiveData<List<Expense>>
 
     @Query("SELECT SUM(expense_amount) as total FROM expense where date BETWEEN :startDay AND :endDay")
